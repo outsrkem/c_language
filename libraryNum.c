f5ee953abde386b06edb9a557a8b33c0fbf2cdef
Outsrkem <xhdascnf@126.com>
2020-09-02 19:36:23 +0800

    优化求质数

--- libraryNum.c
+++ libraryNum.c
@@ -8,14 +8,14 @@
 BOOL flag = TRUE;
 /**
  * 求100以内的素数。
- * 编译命令：指定c99标准，指定sqrt函数库位置
- * gcc -std=c99 libraryNum.c -lm -o libraryNum
+ * 编译命令：指定c89标准，-lm 指定sqrt函数库位置
+ * gcc -std=c89 libraryNum.c -lm -o libraryNum
  * @return
  */
 int main(void) {
     int i,j;
-    for (i = 2; i <= 1000000; i++) {
-        for (j = 2; j < sqrt(i); j++) {
+    for (i = 2; i <= 100; i++) {
+        for (j = 2; j <= sqrt(i); j++) {
             if (i % j == 0) {
                 flag = FALSE;
                 break;
